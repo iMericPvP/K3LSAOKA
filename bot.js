5e4e76c03b902a8ff2ec0321ffc799210c48367c
DKSDAI3sKA <41757098+DKSDAI3sKA@users.noreply.github.com>
2018-08-08 17:32:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const prefix = '$'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$help | $inv | ${client.guilds.size} Servers `,"http://twitch.tv/Noobbot")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -32,6 +31,23 @@ client.login(process.env.BOT_TOKEN);
 
 
  
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = [' $help','$help | $inv','${client.guilds.size} Servers ','${client.users.size} Users','${client.channels.size} Channels'];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/NoobBot`);
+    }, ms);
+
+});
 
 
 client.on('message' , message => {
