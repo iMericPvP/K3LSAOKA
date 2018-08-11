702594a206d36add8fbb6a02919027fb09a16b9d
DKSDAI3sKA <41757098+DKSDAI3sKA@users.noreply.github.com>
2018-08-11 17:02:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '$'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$help | $inv | ${client.guilds.size} Servers `,"http://twitch.tv/Noobbot")
+client.user.setGame(`$help | $inv | ${client.guilds.size} Servers `,"http://twitch.tv/NoobBot")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -2019,7 +2019,6 @@ message.channel.send('**اديك 15 ثاني�� لتوجد العاصمه ا�
 }
 });
 
-const fs = require("fs")
 
 let points = JSON.parse(fs.readFileSync('./Points.json', 'utf8'));
 client.on('message', message => {
