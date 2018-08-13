869a20958bfc3d388c8f50e758b03fb66f9271a2
DKSDAI3sKA <41757098+DKSDAI3sKA@users.noreply.github.com>
2018-08-14 00:00:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -227,6 +227,376 @@ client.on("message", message => {
     
    }
    }); 
+   
+   
+   
+   
+   const Canvas = require("canvas")
+const fs = require("fs")
+const jimp = require("jimp")
+
+let sw = JSON.parse(fs.readFileSync("./setWlc.json", "UTF8"))
+
+ 
+
+    client.on('message', message => {
+
+
+ 
+
+        let mothed = ['text', 'embed', 'image'];
+
+        let sets = message.content.split(" ").slice(1).join(" ")
+
+        let style = message.content.split(" ").slice(2).join(" ")
+
+        let stym = message.content.split(" ").slice(3).join(" ")
+
+        let msz = message.content.split(" ").slice(2).join(" ")
+
+        let ch = message.content.split(" ").slice(2).join(" ")
+
+        let r = message.content.split(" ").slice(4).join(" ")
+
+ 
+
+ 
+
+        if(message.content.startsWith(prefix + "setWlc")) {
+
+    if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**You need `Manage Channels` permission**")
+
+            if(!sw[message.guild.id]) sw[message.guild.id] = {
+
+                cha: "welcome",
+
+                msz: "Welcome Bro",
+
+                styler: "text"
+
+            };
+
+ 
+
+            if(!sets) {
+
+                message.channel.send(`**للستخدام:
+
+            ${prefix}setWlc style <text, image, embed>
+
+            ${prefix}setWlc msg <message>
+
+            ${prefix}setWlc channal <channel name>**`)
+
+            }
+
+ 
+
+            if(!mothed) {
+
+                message.channel.send(`*للستخدام: ${prefix}setWlc style <text, imgae, embed>**`)
+
+            }
+
+ 
+
+            if(message.content === prefix + 'setWlc style image') {
+
+                if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**تحتاج صلاحيه MANAGE_CHANNELS**")
+
+                sw[message.guild.id].styler = 'image'
+
+                message.channel.send(`**تم تغير الاستايل لي  ${sw[message.guild.id].styler}**`)
+
+            }
+
+ 
+
+            if(message.content === prefix + 'setWlc style embed') {
+
+                if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**تحتاج صلاحيه MANAGE_CHANNELS**")
+
+                 sw[message.guild.id].styler = 'embed'
+
+                message.channel.send(`**تم تغير الاستايل لي ${sw[message.guild.id].styler}**`)            }
+
+ 
+
+            if(message.content === prefix + 'setWlc style text') {
+
+                if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**تحتاج صلاحيه MANAGE_CHANNELS**")
+
+                 sw[message.guild.id].styler = 'text'
+
+                message.channel.send(`**تم تغير الاستايل الي ${sw[message.guild.id].styler}**`)
+
+            }
+
+ 
+
+        }
+
+ 
+
+        if(message.content.startsWith(prefix + "setWlc msg")) {
+
+            if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**تحتاج صلاحيه MANAGE_CHANNELS**")
+
+            if(!msz) {
+
+                message.channel.send("للستخدام: $setWlc msg <message>")
+
+            } else {
+
+                message.channel.send(`**تم تغير رساله الترحيب الي __${msz}__**`)
+
+                sw[message.guild.id].msk = msz
+
+            }
+
+        }
+
+ 
+
+        if(message.content.startsWith(prefix + "setWlc channel")) {
+
+            if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**تحتاج صلاحيه MANAGE_CHANNELS**")
+
+            if(!ch) {
+
+                message.channel.send("للستخدام: $setWlc channel <channel name>")
+
+            }
+
+            let chn = message.guild.channels.find("name", ch)
+
+            if(!chn) {
+
+                message.channel.send("**لا يوجد روم بهذه الاسم**")
+
+            }
+
+            else {
+
+                 sw[message.guild.id].cha = chn.name
+
+                 message.channel.send(`**تم تغير روم الترحيب الي __${chn.name}__**`)
+
+                 }
+
+        }
+
+ 
+
+        fs.writeFile('./setWlc.json', JSON.stringify(sw), (err) => {
+
+if (err) console.error(err);
+
+})
+
+})
+
+ 
+
+ 
+
+client.on('guildMemberAdd', member => {
+
+    let channel = member.guild.channels.find("name", sw[member.guild.id].cha)
+
+ 
+
+    if(sw[member.guild.id].styler === "text") {
+
+        channel.sendMessage(`<@${member.user.id}>, ${sw[member.guild.id].msk}`)
+
+    }
+
+ 
+
+    if(sw[member.guild.id].styler === "embed") {
+
+ 
+
+        const embed = new Discord.RichEmbed()
+
+        .setTitle("Member joind.")
+
+        .setColor("GREEN")
+
+        .setThumbnail(member.user.avatarURL)
+
+        .setDescription(`**${sw[member.guild.id].msk}**`)
+
+        .addField("**Member name**", `[<@${member.user.id}>]`,true)
+
+        .addField("**Now we are**", `[${member.guild.memberCount}]`,true)
+
+        channel.sendMessage(`<@${member.user.id}>`)
+
+        channel.sendEmbed(embed)
+
+    }
+
+ 
+
+    if(sw[member.guild.id].styler === "image") {
+
+        if (member.user.bot) return;
+
+const w = ['./setWelcome.png'];
+
+        let Image = Canvas.Image,
+
+            canvas = new Canvas(749, 198),
+
+            ctx = canvas.getContext('2d');
+
+        ctx.patternQuality = 'bilinear';
+
+        ctx.filter = 'bilinear';
+
+        ctx.antialias = 'subpixel';
+
+        ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+
+        ctx.shadowOffsetY = 2;
+
+        ctx.shadowBlur = 2;
+
+        ctx.stroke();
+
+        ctx.beginPath();
+
+ 
+
+        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+
+            if (err) return console.log(err);
+
+            let BG = Canvas.Image;
+
+            let ground = new Image;
+
+            ground.src = Background;
+
+            ctx.drawImage(ground, 0, 0, 749, 198);
+
+ 
+
+})
+
+ 
+
+                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
+
+                jimp.read(url, (err, ava) => {
+
+                    if (err) return console.log(err);
+
+                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+
+                 if (err) return console.log(err);
+
+ 
+
+ctx.font = '35px Aeland';
+
+                        ctx.fontSize = '40px';
+
+                        ctx.fillStyle = "#FFFFFF";
+
+                        ctx.textAlign = "center";
+
+
+ 
+
+                        //ur name
+
+                        ctx.font = '40px Impact';
+
+                        ctx.fontSize = '48px';
+
+                        ctx.fillStyle = "#FFFFFF";
+
+                        ctx.textAlign = "center";
+
+                        ctx.fillText(member.user.username, 420, 100);
+
+ 
+
+                         ctx.font = '30px Impact';
+
+                        ctx.fontSize = '20px';
+
+                        ctx.fillStyle = "#FFFFFF";
+
+                        ctx.textAlign = "center";
+
+                        ctx.fillText(sw[member.guild.id].msk, 410, 170);
+
+ 
+
+ 
+
+                        //Avatar
+
+                        let Avatar = Canvas.Image;
+
+                              let ava = new Avatar;
+
+                              ava.src = buf;
+
+                              ctx.beginPath();
+
+                              ctx.arc(115, 100, 90, 0, Math.PI*2);
+
+                                 ctx.closePath();
+
+                                 ctx.clip();
+
+                                 ctx.drawImage(ava, 5, 5, 200, 200);
+
+              
+ 
+        channel.sendFile(canvas.toBuffer())
+
+ 
+
+ 
+
+ 
+
+})
+
+})
+
+ 
+
+    }
+
+ 
+
+})
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("419553524339179530");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+
+
+
+
 
 
 client.on("message", message => {
@@ -943,350 +1313,7 @@ moment.locale('ar-TN');
 
 
 
-const Canvas = require("canvas")
-const fs = require("fs")
-const jimp = require("jimp")
 
-let sw = JSON.parse(fs.readFileSync("./setLeave.json", "UTF8"))
-
- 
-
-    client.on('message', message => {
-
-
- 
-
-        let mothed = ['text', 'embed', 'image'];
-
-        let setsleave = message.content.split(" ").slice(1).join(" ")
-
-        let stylerleave = message.content.split(" ").slice(2).join(" ")
-
-        let stymleave = message.content.split(" ").slice(3).join(" ")
-
-        let mszleave = message.content.split(" ").slice(2).join(" ")
-
-        let chaleave = message.content.split(" ").slice(2).join(" ")
-
-        let rleave = message.content.split(" ").slice(4).join(" ")
-
- 
-
- 
-
-        if(message.content.startsWith(prefix + "setLeave")) {
-
-    if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**تحتاج صلاحيه**")
-
-            if(!sw[message.guild.id]) sw[message.guild.id] = {
-
-                cha: "welcome",
-
-                msz: "Welcome Bro",
-
-                styler: "text"
-
-            };
-
- 
-
-            if(!setsleave) {
-
-                message.channel.send(`**للستخدام:
-            ${prefix}setLeave style <text, image, embed>
-            ${prefix}setLeave msg <message>
-            ${prefix}setLeave channal <channel name>**`)
-
-            }
-
- 
-
-            if(!mothed) {
-
-                message.channel.send(`**للستخدام: ${prefix}setLeave style <text, imgae, embed>**`)
-
-            }
-
- 
-
-            if(message.content === prefix + 'setLeave style image') {
-
-                if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**تحتاج صلاحيه MANAGE_CHANNELS**")
-
-                sw[message.guild.id].stylerleave = 'image'
-
-                message.channel.send(`**تم تغير الي ${sw[message.guild.id].styler}**`)
-
-            }
-
- 
-
-            if(message.content === prefix + 'setLeave style embed') {
-
-                if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**تحتاج صلاحيه MANAGE_CHANNELS**")
-
-                 sw[message.guild.id].stylerleave = 'embed'
-
-                message.channel.send(`**تم تغير الي ${sw[message.guild.id].styler}**`)            }
-
- 
-
-            if(message.content === prefix + 'set style text') {
-
-                if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**تحتاج صلاحيه MANAGE_CHANNELS**")
-
-                 sw[message.guild.id].stylerleave = 'text'
-
-                message.channel.send(`**تم تغير الي  ${sw[message.guild.id].styler}**`)
-
-            }
-
- 
-
-        }
-
- 
-
-        if(message.content.startsWith(prefix + "setLeave msg")) {
-
-            if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**تحتاج صلاحيه MANAGE_CHANNELS**")
-
-            if(!msz) {
-
-                message.channel.send("للستخدام: $setLeave msg <message>")
-
-            } else {
-
-                message.channel.send(`**تم تغير الي __${msz}__**`)
-
-                sw[message.guild.id].msk = msz
-
-            }
-
-        }
-
- 
-
-        if(message.content.startsWith(prefix + "setLeave channel")) {
-
-            if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**تحتاج صلاحيه MANAGE_CHANNELS**")
-
-            if(!ch) {
-
-                message.channel.send("للستخدام: $setLeave channel <channel name>")
-
-            }
-
-            let chn = message.guild.channels.find("name", ch)
-
-            if(!chn) {
-
-                message.channel.send("**لا ينكنني ان ارا هذه الروم**")
-
-            }
-
-            else {
-
-                 sw[message.guild.id].cha = chn.name
-
-                 message.channel.send(`**تم تغير الي __${chn.name}__**`)
-
-                 }
-
-        }
-
- 
-
-        fs.writeFile('./setWlc.json', JSON.stringify(sw), (err) => {
-
-if (err) console.error(err);
-
-})
-
-})
-
- 
-
- 
-
-client.on('guildMemberAdd', member => {
-
-    let channel = member.guild.channels.find("name", sw[member.guild.id].cha)
-
- 
-
-    if(sw[member.guild.id].styler === "text") {
-
-        channel.sendMessage(`<@${member.user.id}>, ${sw[member.guild.id].msk}`)
-
-    }
-
- 
-
-    if(sw[member.guild.id].styler === "embed") {
-
- 
-
-        const embed = new Discord.RichEmbed()
-
-        .setTitle("Member joind.")
-
-        .setColor("GREEN")
-
-        .setThumbnail(member.user.avatarURL)
-
-        .setDescription(`**${sw[member.guild.id].msk}**`)
-
-        .addField("**Member name**", `[<@${member.user.id}>]`,true)
-
-        .addField("**Now we are**", `[${member.guild.memberCount}]`,true)
-
-        channel.sendMessage(`<@${member.user.id}>`)
-
-        channel.sendEmbed(embed)
-
-    }
-
- 
-
-    if(sw[member.guild.id].styler === "image") {
-
-        if (member.user.bot) return;
-
-const w = ['./setwelcome.png'];
-
-        let Image = Canvas.Image,
-
-            canvas = new Canvas(749, 198),
-
-            ctx = canvas.getContext('2d');
-
-        ctx.patternQuality = 'bilinear';
-
-        ctx.filter = 'bilinear';
-
-        ctx.antialias = 'subpixel';
-
-        ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-
-        ctx.shadowOffsetY = 2;
-
-        ctx.shadowBlur = 2;
-
-        ctx.stroke();
-
-        ctx.beginPath();
-
- 
-
-        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-
-            if (err) return console.log(err);
-
-            let BG = Canvas.Image;
-
-            let ground = new Image;
-
-            ground.src = Background;
-
-            ctx.drawImage(ground, 0, 0, 749, 198);
-
- 
-
-})
-
- 
-
-                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
-
-                jimp.read(url, (err, ava) => {
-
-                    if (err) return console.log(err);
-
-                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-
-                 if (err) return console.log(err);
-
- 
-
-ctx.font = '35px Aeland';
-
-                        ctx.fontSize = '40px';
-
-                        ctx.fillStyle = "#FFFFFF";
-
-                        ctx.textAlign = "center";
-
-
- 
-
-                        //ur name
-
-                        ctx.font = '40px Impact';
-
-                        ctx.fontSize = '48px';
-
-                        ctx.fillStyle = "#FFFFFF";
-
-                        ctx.textAlign = "center";
-
-                        ctx.fillText(member.user.username, 420, 100);
-
- 
-
-                         ctx.font = '30px Impact';
-
-                        ctx.fontSize = '20px';
-
-                        ctx.fillStyle = "#FFFFFF";
-
-                        ctx.textAlign = "center";
-
-                        ctx.fillText(sw[member.guild.id].msk, 410, 170);
-
- 
-
- 
-
-                        //Avatar
-
-                        let Avatar = Canvas.Image;
-
-                              let ava = new Avatar;
-
-                              ava.src = buf;
-
-                              ctx.beginPath();
-
-                              ctx.arc(115, 100, 90, 0, Math.PI*2);
-
-                                 ctx.closePath();
-
-                                 ctx.clip();
-
-                                 ctx.drawImage(ava, 5, 5, 200, 200);
-
-              
- 
-        channel.sendFile(canvas.toBuffer())
-
- 
-
- 
-
- 
-
-})
-
-})
-
- 
-
-    }
-
- 
-
-})
 
 
    const ms = require("ms");
