8eaffae6d4c743bce2af9478ace120cfad63582c
DKSDAI3sKA <41757098+DKSDAI3sKA@users.noreply.github.com>
2018-08-17 02:24:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -837,9 +837,11 @@ client.on('message', function(msg) {
       .setTitle(`Noobbot`)
       .addField('عدد الرتب',`[** __${msg.guild.roles.size}__ **]`,true)
       .addField('عدد الاعضاء',`[** __${msg.guild.memberCount}__ **]`,true)
+      .addField('AFKروم الـ',`[** __${message.guild.afkChannel}__ **]`,true)
       .addField('الرومات الكتابيه',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
       .addField('الرومات الصوتيه',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
 	  .addField('اونر السيرفر',`[** __${msg.guild.owner}__ **]`,true)
+	  .addField('اخر عضو دخل السيرفر',`[** __${Array.from(message.channel.guild.members.values()).sort((a, b) => b.joinedAt - a.joinedAt).map(m => `<@!${m.id}>`).splice(0, 1)}__ **]`,true)
       .addField('صنع السيرفر في',msg.guild.createdAt.toLocaleString())
 	  .setFooter("لكي تعرف حالات الاعضاء  قم بكتابه $members | لكي تعرف ماهي رتب السيرفر قم بكتابه $ranks")  
       msg.channel.send({embed:embed});
