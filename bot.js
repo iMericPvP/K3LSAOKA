adddb5bb5bbe44a662a29e2dddce33b04a44858a
DKSDAI3sKA <41757098+DKSDAI3sKA@users.noreply.github.com>
2018-08-18 20:41:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,6 +32,43 @@ client.login(process.env.BOT_TOKEN);
 
 
 
+client.on('message', msg => { 
+if (msg.content.startsWith(`$sug`)) {
+// تعريف الارجس
+   let args = msg.content.split(" ").slice(1);
+// لو ما منشن احد يرد عيله
+  if (!msg.mentions.members.first()) return msg.reply(`يجب عليك منشن شخص`)
+// لو ما كتب تبليغ بيقوله اكتب تبليغ
+  if (!args[1]) return msg.reply(`اكتب تبيلغك`)
+// استبدل <الروم> بأسم الروم حقك
+    msg.guild.channels.find('name', 'suggests').send(`
+  تبليغ على : ${msg.mentions.members.first()}
+  بلغ عليه من قبل : ${msg.member}
+  في روم : ${msg.channel.name}
+  السبب : **${args.join(" ").split(msg.mentions.members.first()).slice(' ')}**
+  `)
+  }
+}
+})
+
+client.on('message', msg => { 
+if (msg.content.startsWith(`$report`)) {
+// تعريف الارجس
+   let args = msg.content.split(" ").slice(1);
+// لو ما منشن احد يرد عيله
+  if (!msg.mentions.members.first()) return msg.reply(`يجب عليك منشن شخص`)
+// لو ما كتب تبليغ بيقوله اكتب تبليغ
+  if (!args[1]) return msg.reply(`اكتب تبيلغك`)
+// استبدل <الروم> بأسم الروم حقك
+    msg.guild.channels.find('name', 'report').send(`
+  تبليغ على : ${msg.mentions.members.first()}
+  بلغ عليه من قبل : ${msg.member}
+  في روم : ${msg.channel.name}
+  السبب : **${args.join(" ").split(msg.mentions.members.first()).slice(' ')}**
+  `)
+  }
+}
+})
 
 
 
